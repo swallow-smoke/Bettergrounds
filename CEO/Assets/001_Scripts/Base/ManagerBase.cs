2f831dcaea479bc1014cb563a0203b2b30dc9784
teatime <khj10004534@gmail.com>
2026-04-24 14:48:23 +0900

    Add | Base Set
    Manager base setting

---
+++ CEO/Assets/001_Scripts/Base/ManagerBase.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Base
+{
+    public abstract class ManagerBase
+    {
+        public virtual void Initialize()
+        {
+            Debug.Log($"Initialize {GetType().Name}");
+        }
+    }
+}
---
+++ CEO/Assets/001_Scripts/Manager/GManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Base;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace _001_Scripts.Manager
+{
+    public class GManager : ManagerBase
+    {
+        [SerializeField] private Dictionary<string, ManagerBase> _managers;
+        
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            foreach (var manager in _managers)
+            {
+                manager.Value.Initialize();
+            }
+        }
+
+        public ManagerBase GetManager(string key)
+        {
+            var manager = _managers.ContainsKey(key) ? _managers[key] : null;
+            return manager;
+        }
+    }
+}
